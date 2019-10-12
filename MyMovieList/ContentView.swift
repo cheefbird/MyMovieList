b6e7437536e70b19580de46175e34d60faf876d0
Francis Breidenbach <fbreidenbach@gmail.com>
2019-10-12 00:12:37 -0700

    Initial Commit

---
+++ MyMovieList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyMovieList
+//
+//  Created by Francis Breidenbach on 10/12/19.
+//  Copyright © 2019 Francis Breidenbach. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
