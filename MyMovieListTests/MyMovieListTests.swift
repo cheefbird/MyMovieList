acc9fdde861fbf3fddb1fee7340be292fcaa1a26
Francis Breidenbach <fbreidenbach@gmail.com>
2019-10-27 23:33:48 -0700

    Add test target

---
+++ MyMovieListTests/MyMovieListTests.swift
@@ -0,0 +1,33 @@
+//
+//  MyMovieListTests.swift
+//  MyMovieListTests
+//
+//  Created by Francis Breidenbach on 10/27/19.
+//  Copyright © 2019 Francis Breidenbach. All rights reserved.
+//
+
+import XCTest
+
+class MyMovieListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
